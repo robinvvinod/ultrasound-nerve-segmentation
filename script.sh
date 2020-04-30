82aac832ca8d934cc0925f60d4c6899012949731
Robin Vinod <robinvvinod@gmail.com>
2020-04-30 11:06:13 +0800

    Update hyperparameters

--- script.sh
+++
@@ -1,7 +0,0 @@
-import os 
-import sys
-import subprocess
-
-!rm -rf /kaggle/working/*
-!git clone https://github.com/robinvvinod/ultrasound-nerve-segmentation-keras
-!mv ultrasound-nerve-segmentation-keras/* /kaggle/working; rm -rf ultrasound-nerve-segmentation-keras; rm train.py
