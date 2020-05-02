b9c2ea75a0bd2a463c5af13cd815752d876d8195
Robin Vinod <robinvvinod@gmail.com>
2020-05-02 18:30:40 +0800

    removed dropout

--- network.py
+++ network.py
@@ -1,7 +1,6 @@
 # yapf: disable
 from keras.models import Model
 from keras.layers.convolutional import Conv2D
-from keras.layers import SpatialDropout2D
 from layers2D import *
 
 # Use the functions provided in layers3D to build the network
@@ -11,37 +10,29 @@ def network(input_img, n_filters=16, dropout=0.5, batchnorm=True):
     # contracting path
     
     c0 = residual_block(input_img, n_filters=n_filters, batchnorm=batchnorm, strides=1, recurrent=2)
-    p0 = SpatialDropout2D(dropout * 0.5)(c0)
 
-    c1 = residual_block(p0, n_filters=n_filters * 2, batchnorm=batchnorm, strides=2, recurrent=2)
-    p1 = SpatialDropout2D(dropout)(c1)
+    c1 = residual_block(c0, n_filters=n_filters * 2, batchnorm=batchnorm, strides=2, recurrent=2)
 
-    c2 = residual_block(p1, n_filters=n_filters * 4, batchnorm=batchnorm, strides=2, recurrent=2)
-    p2 = SpatialDropout2D(dropout)(c2)
+    c2 = residual_block(c1, n_filters=n_filters * 4, batchnorm=batchnorm, strides=2, recurrent=2)
 
-    c3 = residual_block(p2, n_filters=n_filters * 8, batchnorm=batchnorm, strides=1, recurrent=2)
-    p3 = SpatialDropout2D(dropout)(c3)
+    c3 = residual_block(c2, n_filters=n_filters * 8, batchnorm=batchnorm, strides=1, recurrent=2)
     
     # bridge
     
-    b0 = residual_block(p3, n_filters=n_filters * 16, batchnorm=batchnorm, strides=1, recurrent=2)
+    b0 = residual_block(c3, n_filters=n_filters * 16, batchnorm=batchnorm, strides=1, recurrent=2)
 
     # expansive path
     
-    #attn0 = AttnGatingBlock(p3, b0, n_filters * 16)
-    u0 = transpose_block(b0, p3, n_filters=n_filters * 8)
-    d0 = SpatialDropout2D(dropout)(u0)
+    attn0 = AttnGatingBlock(c3, b0, n_filters * 16)
+    u0 = transpose_block(b0, attn0, n_filters=n_filters * 8)
     
-    #attn1 = AttnGatingBlock(p2, d0, n_filters * 8)
-    u1 = transpose_block(d0, p2, n_filters=n_filters * 4)
-    d1 = SpatialDropout2D(dropout)(u1)
+    attn1 = AttnGatingBlock(c2, u0, n_filters * 8)
+    u1 = transpose_block(u0, attn1, n_filters=n_filters * 4)
     
-    #attn2 = AttnGatingBlock(p1, d1, n_filters * 4)
-    u2 = transpose_block(d1, p1, n_filters=n_filters * 2)
-    d2 = SpatialDropout2D(dropout)(u2)
+    attn2 = AttnGatingBlock(c1, u1, n_filters * 4)
+    u2 = transpose_block(u1, attn2, n_filters=n_filters * 2)
     
-    u3 = transpose_block(d2, p0, n_filters=n_filters)  
-    d3 = SpatialDropout2D(dropout)(u3)
+    u3 = transpose_block(u2, c0, n_filters=n_filters)  
 
     outputs = Conv2D(filters=1, kernel_size=1, strides=1, activation='sigmoid')(d3)
     model = Model(inputs=[input_img], outputs=[outputs])
